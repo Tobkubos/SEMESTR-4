7591a13c920f541e4aeaeefaabf45f9c9c811b83
Tobkubos <128313959+Tobkubos@users.noreply.github.com>
2024-02-29 16:45:01 +0100

    qewqewqe

--- AP/helix.cpp
+++ AP/helix.cpp
@@ -25,31 +25,91 @@ vector<pair<int,int>> CheckIntersectionPoints(const vector<int>& vector1, const 
     return Intersections;
 }
 
+
+int Sumator(vector<pair<int,int>> Intersections, vector<int> vec1, vector<int> vec2){
+    int sum = 0;
+
+    int v2;
+    int v1;
+    int start1 = 0; 
+    int start2 = 0;
+    int end1 = 0;
+    int end2 = 0;
+
+
+        //STARTOWE DO PIERWSZEGO PRZECIECIA
+        for(int q = 0; q< Intersections[0].first; q++){
+            start1 += vec1[q];
+        }
+        for(int w = 0; w< Intersections[0].second; w++){
+            start2 += vec2[w];
+        }
+
+        //KOŃCE WSTĘG
+        for(int e = Intersections[Intersections.size()-1].first; e < vec1.size(); e++){
+            end1 += vec1[e];
+        }
+        for(int r = Intersections[Intersections.size()-1].second; r < vec2.size(); r++){
+            end2 += vec2[r];
+        }
+        
+        if(start1>start2){
+            sum += start1;
+        }
+        else{
+            sum += start2;
+        }
+
+
+        if(end1>end2){
+            sum += end1;
+        }
+        else{
+            sum += end2;
+        }
+
+    cout<<"start1: "<<start1<<"   start2: "<<start2<<endl;
+    cout<<"end1: "<<end1<<"   end2: "<<end2<<endl;
+
+    //MIEDZY PUNKTAMI PRZECIECIA
+    for(int i=0; i<Intersections.size()-1; i++){
+        v1 = 0;
+        v2 = 0;
+
+        for(int j=Intersections[i].first; j<Intersections[i+1].first; j++){
+            v1 += vec1[j];
+        }
+        for(int k=Intersections[i].second; k<Intersections[i+1].second; k++){
+            v2 += vec2[k];
+        }
+
+        if(v1>v2){
+            sum+=v1;
+        }
+        else{
+            sum+=v2;
+        }
+        cout<<"sciezka1: "<<v1<<"   sciezka2: "<<v2<<endl;
+    
+    }
+
+
+    return sum;
+}
+
+
+
+
+
+
 int main(){
 
 
-    vector<int> vector1;
-    vector<int> vector2;
+    vector<int> vector1 = {3,5,7,9,20,25,30,40,55,56,57,60,62};
+    vector<int> vector2 = {1,4,7,11,14,25,44,47,55,57,100};
 
-    vector1.push_back(1);   //0
-    vector1.push_back(3);   //1
-    vector1.push_back(12);  //2
-    vector1.push_back(26);  //3
-    vector1.push_back(40);  //4
-
-
-
-    vector2.push_back(0);   //0
-    vector2.push_back(2);   //1
-    vector2.push_back(12);  //2
-    vector2.push_back(13);  //3
-    vector2.push_back(14);  //4
-    vector2.push_back(15);  //5
-    vector2.push_back(16);  //6
-    vector2.push_back(26);  //7
-    vector2.push_back(30);  //8
-    vector2.push_back(40);  //9
-    CheckIntersectionPoints(vector1, vector2);
-    
+    vector<pair<int,int>> Intersections = CheckIntersectionPoints(vector1, vector2);
+    int Sum = Sumator(Intersections, vector1, vector2);
+    cout<<Sum<<endl;   
     return 0;
 }
