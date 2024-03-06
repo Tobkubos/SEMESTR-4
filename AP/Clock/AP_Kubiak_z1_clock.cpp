e8df299b763bfd65f0a94d6d1434bfa0cc5914b2
Tobkubos <128313959+Tobkubos@users.noreply.github.com>
2024-03-06 11:42:19 +0100

    qwertyui

--- AP/Clock/AP_Kubiak_z1_clock.cpp
+++ AP/Clock/AP_Kubiak_z1_clock.cpp
@@ -38,12 +38,14 @@ int main(){
         cin>>h1>>m1>>s1>>u1;
         cin>>h2>>m2>>s2>>u2;
         cin>>radius;
-            if(radius > 0 && radius < 10000){
+            if(radius > 0 && radius <= 10000){
+                if((h1 >= 0 && h1 <12) && (m1 >=0 && m1 < 60) && (s1 >=0 && s1 < 60) && (u1 >=0 && u1 <100) &&
+                (   h2 >= 0 && h2 <12) && (m2 >=0 && m2 < 60) && (s2 >=0 && s2 < 60) && (u2 >=0 && u2 <100)){
                 int time1 = CalcTime(h1,m1,s1,u1);
                 int time2 = CalcTime(h2,m2,s2,u2);
                 if(time1 < time2){
                 CalcArea(time1, time2, radius, i, DeltaMax);
-            }
+            }}
         }
     }
 }
--- AP/Penalty/AP_Kubiak_z1_penalty.cpp
+++ AP/Penalty/AP_Kubiak_z1_penalty.cpp
@@ -3,33 +3,23 @@
 
 using namespace std;
 
-int Factorial(int a){
-    int sum = 1;
-    for(int i = 0; i<=a; i++){
-        sum *= a;
-        a--;
-    }
-    return sum;
-}
-
-int MatchesInGroup(int teams){
-    int GroupSum = 0;
-    GroupSum = Factorial(teams-1);
+long MatchesInGroup(long teams){
+    long GroupSum = (teams * (teams-1))/2;
     return GroupSum;
 }
 
-int checkIfPowerOfTwo(int x){
+long checkIfPowerOfTwo(long x){
     while(x%2==0){
         x = x/2;
     }
     return x;
 }
 
-int checkKnockout(int KnockoutCountries){
-    int check = checkIfPowerOfTwo(KnockoutCountries);
+long checkKnockout(long KnockoutCountries){
+    long check = checkIfPowerOfTwo(KnockoutCountries);
     //cout<<"check: "<<check<<endl;
     if(check != 1){
-        int temp = 1;
+        long temp = 1;
         while(temp < KnockoutCountries){
             temp = temp*2;
         }
@@ -39,36 +29,40 @@ int checkKnockout(int KnockoutCountries){
     return KnockoutCountries;
 }
 
-int AddTeams(int check, int KnockoutCountries){
+long AddTeams(long check, long KnockoutCountries){
     if(check ==0){return 0;}
     else{
-    int neededTeamsInKnockout = check - KnockoutCountries;
+    long neededTeamsInKnockout = check - KnockoutCountries;
     //cout<<"Needed Teams in Knockout: "<<neededTeamsInKnockout<<endl;
     return neededTeamsInKnockout;
 }}
 
 int main(){
-    int Groups;
-    int TeamsInGroup;
-    int Advance;
-    int DirectAdv;
+    long Groups;
+    long TeamsInGroup;
+    long Advance;
+    long DirectAdv;
 
     while(!cin.eof()){
         cin>>Groups>>TeamsInGroup>>Advance>>DirectAdv;
 
-        int GroupStageMatches = 0;
-        int KnockoutMatches = 0;
-        int KnockoutCountries = (Groups*Advance)+DirectAdv;
+        long long GroupStageMatches = 0;
+        long KnockoutMatches = 0;
+        long KnockoutCountries = (Groups*Advance)+DirectAdv;
 
         
 
-        if(Groups>0 && TeamsInGroup > 0 && Advance > 0 && DirectAdv >= 0 && Advance <= TeamsInGroup){
+        if(Groups>0 && 
+           TeamsInGroup > 0 && 
+           Advance > 0 && 
+           DirectAdv >= 0 && 
+           Advance <= TeamsInGroup){
         //SPRAWDZ CZY KNOCKOUT STAGE jest potęgą 2'jki
-        int check = checkKnockout(KnockoutCountries);
+        long check = checkKnockout(KnockoutCountries);
         //Ile trzeba dodac do fazy knockout aby turniej mial sens?
-        int neeededTeams = AddTeams(check, KnockoutCountries);
+        long neeededTeams = AddTeams(check, KnockoutCountries);
 
-        int KC = check;
+        long KC = check;
         GroupStageMatches = Groups * MatchesInGroup(TeamsInGroup);
         while(KC>0){
             KnockoutMatches += KC/2;
@@ -81,6 +75,7 @@ int main(){
         cout<<"Advance: "<<Advance<<endl;
         cout<<"Group Matches: "<<GroupStageMatches<<endl;
         cout<<"Knockout Countries: "<<KnockoutCountries<<endl;
+        //65536 65536 65536 0
         */
 
         cout<<Groups<<"*"<<Advance<<"/"<<TeamsInGroup<<"+"<<DirectAdv<<"="<<KnockoutMatches+GroupStageMatches<<"+"<<neeededTeams<<endl;
