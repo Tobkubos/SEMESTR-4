727ce397f7565694366d8b179c6b69cf1c34c40f
Tobkubos <128313959+Tobkubos@users.noreply.github.com>
2024-03-13 11:50:56 +0100

    qwertyup

---
+++ AP/Cipher/cipher.cpp
@@ -0,0 +1,38 @@
+// CIPHER - SZYFROWANIE
+// 1. szyfr podstawieniowy - jeden znak zastepujemy innym
+// wazne jest to co jest aflfabetem
+// A -znaków - pierwsze znaki alfabetu, tylko małe litery
+// od (a do - N'tej literki)
+// abcde
+// nieszyfrowane - prz. leksykograficzny
+// cebdbac
+// cac
+// ecd  
+// dca
+// aba
+// bac
+//
+// zaszyfrowane - cedab
+//
+// pierwsze jest slowo na c wiec usawiany c w tablicy na pierwsza pozycje, itd az do utrworzenia alfabetu
+// c nie musi byc pierwsze, ale ogolnie musi byc, co jest wczesniej co jest pozniej 
+// K co najmniej rowne A+1
+// 
+// 
+// robimy rownania liter jak z alfabetu
+// np. d < c
+// potem sprawdzamy ktorej litery nie ma po prawej stronie
+// i tej której nie ma zapisujemy do tablicy
+//
+// GRAF SKIEROWANY i sortowanie topologiczne - EZ WIN
+// Algorytm Kanna
+//
+// brak odpowiedzi gdy w grafie nie ma wsyzstkich znakow
+// lub wystepuje cykl
+//
+//
+//
+//
+//
+//
+//
---
+++ AP/Reverse/reverse.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int a;
+    int b;
+
+    cin>>a;
+    cin>>b;
+
+
+    cout<<"Hello world";
+
+    return 0;
+}
