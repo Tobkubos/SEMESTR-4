1f05b10253aaca77111d8c24e37325e81c3e198a
Tobkubos <128313959+Tobkubos@users.noreply.github.com>
2024-03-13 17:05:00 +0100

    rtyuio

--- AP/Reverse/reverse.cpp
+++ AP/Reverse/reverse.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 
 int main(){
+    unsigned long long cases = 0;
+    cin>>cases;
 
-    int a;
-    int b;
+    for(int i =0; i<cases; i++){
+        long long a=0;
+        long long b=0;
 
-    cin>>a;
-    cin>>b;
+        cin>>a>>b;
 
+        string tempA = to_string(a);
+        string tempB = to_string(b);
+        long long intA = stoi(string(tempA.rbegin(), tempA.rend()));
+        long long intB = stoi(string(tempB.rbegin(), tempB.rend()));
+        long long intC = intA + intB;
 
-    cout<<"Hello world";
-
+        string tempC = to_string(intC);
+        long long intSUM = stoi(string(tempC.rbegin(), tempC.rend()));
+        cout<<intSUM<<endl;
+    } 
     return 0;
 }
