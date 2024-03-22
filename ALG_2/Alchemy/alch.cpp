6fd6b66f45cca7e37d88f8a23a396e3fac624864
Tobkubos <tobkub7@gmail.com>
2024-03-22 21:39:15 +0100

    yui

--- ALG_2/Alchemy/alch.cpp
+++ ALG_2/Alchemy/alch.cpp
@@ -1,12 +1,48 @@
 #include <iostream>
-
+#include <vector>
+#include <map>
 using namespace std;
 
-int main(){
+int main()
+{
+    int cases = 0;
+    cin >> cases;
+    for (int i = 0; i < cases; i++)
+    {
+        map<int, vector<int>> connectionTable;
+        vector<int> priceTable;
+        int potions = 0;
+        int transmutations = 0;
+        cin >> potions >> transmutations;
 
-    int a;
-    cin>>a;
-    cout<<"hello"<<endl;
-
+        for (int k = 0; k < potions; k++)
+        {
+            int price = 0;
+            cin >> price;
+            priceTable.push_back(price);
+        }
+        for (int j = 0; j < transmutations; j++)
+        {
+            int key = 0;
+            int value = 0;
+            cin >> key >> value;
+            connectionTable[key].push_back(value);
+        }
+        cout << endl;
+        for (auto q : priceTable)
+        {
+            cout << q << " ";
+        }
+        cout << endl;
+        for (auto x : connectionTable)
+        {
+            cout << x.first << ": ";
+            for (auto y : x.second)
+            {
+                cout << y << " ";
+            }
+            cout << endl;
+        }
+    }
     return 0;
 }
--- AP/Cipher/ciphercopy.cpp
+++ AP/Cipher/ciphercopy.cpp
@@ -2,37 +2,158 @@
 #include <iostream>
 #include <vector>
 #include <set>
-#include <unordered_map>
+#include <map>
 #include <stack>
 
 using namespace std;
 
-void Cipher(vector<string> tab)
+void topologicalSortStep(char v, map<char, vector<char>> &graph, map<char, bool> &visited, stack<char> &s)
 {
-    set<pair<char, char>> pairs;
-    unordered_map<char, vector<char>> graf;
-    ///////////////
-
-    // SPRAWDZANIE
-
-    int longestSize = 0;
-    for (int i = 0; i < tab.size(); i++)
+    // cout << v << " ";
+    visited[v] = true;
+    for (char neighbor : graph[v])
     {
-        int temp = 0;
-        temp = tab[i].size();
-        if (temp > longestSize)
+        if (!visited[neighbor])
         {
-            longestSize = temp;
+            topologicalSortStep(neighbor, graph, visited, s);
         }
     }
+    s.push(v);
+}
 
+void topologicalSort(map<char, vector<char>> &graph, int sizeOfTheAlphabet, vector<char> alphabet, string message)
+{
+    map<char, bool> visited;
+    stack<char> s;
+    //////////
+    // TEST
+    set<char> start1;
+    set<char> start2;
+    for (auto i : graph)
+    {
+        start1.insert(i.first);
+        for (auto j : i.second)
+        {
+            start2.insert(j);
+        }
+    }
+    /*
+        for (auto i : start1)
+        {
+            cout << i << " ";
+        }
+
+        cout << endl;
+
+        for (auto k : start2)
+        {
+            cout << k << " ";
+        }
+        cout << endl;
+    */
+    // ZNAJDUJE WIERZCHOLEK BEZ RODZICOW
+    set<char> notIn;
+
+    for (auto i : start1)
+    {
+        bool found = false;
+        for (auto j : start2)
+        {
+            if (i == j)
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            notIn.insert(i);
+        }
+    }
+    /*
+        for (auto i : notIn)
+        {
+            cout << i << " ";
+        }
+        */
+
+    if (notIn.size() == 1)
+    {
+
+        for (auto i : graph)
+        {
+            visited[i.first] = false;
+        }
+
+        for (auto i : graph)
+        {
+
+            for (auto j : notIn)
+            {
+                if (!visited[i.first])
+                {
+                    topologicalSortStep(j, graph, visited, s);
+                }
+            }
+        }
+
+        if (s.size() == sizeOfTheAlphabet)
+        {
+            // WYPISUJE KOLEJNOSC ALFABETU
+
+            /*
+            while (!s.empty())
+            {
+                cout << s.top() << " ";
+                s.pop();
+            }
+            */
+            map<char, char> DescryptedAlphabet;
+            int i = 0;
+            while (!s.empty())
+            {
+                DescryptedAlphabet[s.top()] = alphabet[i];
+                i++;
+                s.pop();
+            }
+            /*
+            for (auto da : DescryptedAlphabet)
+            {
+                cout << da.first << " " << da.second << endl;
+            }
+            */
+
+            for (int i = 0; i < message.size(); i++)
+            {
+                cout << DescryptedAlphabet[message[i]];
+            }
+            cout << endl;
+        }
+        else
+        {
+            cout << "Message cannot be decrypted" << endl;
+        }
+    }
+    if (notIn.size() != 1)
+    {
+        cout << "Message cannot be decrypted" << endl;
+    }
+}
+
+void Cipher(vector<string> tab, int sizeOfTheAlphabet, vector<char> alphabet, string message)
+{
+    set<pair<char, char>> pairs;
+    map<char, vector<char>> graf;
+
+    // ZALEZNOSCI
     for (int i = 1; i < tab.size(); i++)
     {
         //
         int sizeOfWord1 = tab[i - 1].size();
         int sizeOfWord2 = tab[i].size();
         int SmallestOfPair = 0;
-
+        // cout << sizeOfWord1 << " " << sizeOfWord2 << endl;
+        //
         if (sizeOfWord1 >= sizeOfWord2)
         {
             SmallestOfPair = sizeOfWord2;
@@ -52,29 +173,44 @@ void Cipher(vector<string> tab)
         }
     }
 
+    ///////////////
+
+    // ZALEZNOSCI W GRAFIE
     for (auto para : pairs)
     {
-        cout << para.first << "-->" << para.second << endl;
+        // cout << para.first << "-->" << para.second << endl;
         graf[para.first].push_back(para.second);
     }
-
-    cout << endl
-         << endl;
-
+    /*
+    // WYPIS SASIADOW
     for (auto key : graf)
     {
-        cout << key.first << " ";
+        cout << key.first << ": ";
         for (auto neighbours : key.second)
         {
             cout << neighbours << " ";
         }
         cout << endl;
     }
+    cout << endl
+         << endl
+         << endl;
+    */
+    // SORTOWANIE
+    topologicalSort(graf, sizeOfTheAlphabet, alphabet, message);
 }
 
 int main()
 {
     int cases;
+    vector<char> alphabet = {
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G'};
 
     cin >> cases;
 
@@ -95,7 +231,7 @@ int main()
         }
         string message;
         cin >> message;
-        Cipher(tab);
+        Cipher(tab, SizeOfAlphabet, alphabet, message);
     }
 
     return 0;
