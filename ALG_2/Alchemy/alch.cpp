48ad46ab7ed0e3a65351408d3cf9cee959cc5b24
Tobkubos <tobkub7@gmail.com>
2024-03-23 15:39:31 +0100

    io

--- ALG_2/Alchemy/alch.cpp
+++ ALG_2/Alchemy/alch.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <stack>
+#include <algorithm>
+
 using namespace std;
+
+void AlchemicStep(int v, map<int, vector<int>> &graph, map<int, bool> &visited, stack<int> &s)
+{
+    visited[v] = true;
+    for (char neighbor : graph[v])
+    {
+        if (!visited[neighbor])
+        {
+            AlchemicStep(neighbor, graph, visited, s);
+        }
+    }
+    s.push(v);
+}
+
+void Alchemic(map<int, vector<int>> connectionTable, vector<pair<int, int>> priceTable)
+{
+    map<int, bool> visited;
+    stack<int> s;
+    // TO DO:
+    // ZNAJDZ NAJMNIEJSZY INDEX CENY Z VECTORA PRICETABLE;
+    int minPriceIndex = 3;
+    //
+    for (auto i : connectionTable)
+    {
+        visited[i.first] = false;
+    }
+
+    AlchemicStep(minPriceIndex, connectionTable, visited, s);
+
+    while (!s.empty())
+    {
+        cout << s.top() << " ";
+        s.pop();
+    }
+}
 
 int main()
 {
@@ -10,16 +48,17 @@ int main()
     for (int i = 0; i < cases; i++)
     {
         map<int, vector<int>> connectionTable;
-        vector<int> priceTable;
+        vector<pair<int, int>> priceTable;
         int potions = 0;
         int transmutations = 0;
         cin >> potions >> transmutations;
 
-        for (int k = 0; k < potions; k++)
+        for (int k = 1; k < potions + 1; k++)
         {
             int price = 0;
             cin >> price;
-            priceTable.push_back(price);
+            pair<int, int> KeyValue(k, price);
+            priceTable.push_back(KeyValue);
         }
         for (int j = 0; j < transmutations; j++)
         {
@@ -31,7 +70,7 @@ int main()
         cout << endl;
         for (auto q : priceTable)
         {
-            cout << q << " ";
+            cout << q.first << " / " << q.second << endl;
         }
         cout << endl;
         for (auto x : connectionTable)
@@ -43,6 +82,8 @@ int main()
             }
             cout << endl;
         }
+
+        Alchemic(connectionTable, priceTable);
     }
     return 0;
 }
