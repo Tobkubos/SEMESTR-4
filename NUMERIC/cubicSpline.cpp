46f8ebae4a4250262e74f8b0674b997a3387a94b
Tobkubos <tobkub7@gmail.com>
2024-04-01 23:48:10 +0200

    pppp

---
+++ NUMERIC/cubicSpline.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<float> a;
+    vector<float> b;
+    vector<float> c;
+    vector<float> d;
+
+    double t = 0.1; // od 0 do 1
+    vector<float> y = {1, 4, 12, 16, 21};
+    cout << y[1];
+    return 0;
+}
