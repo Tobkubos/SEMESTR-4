c179f4a831355ce82abc782bf69b17cf2b3ff975
Tobkubos <128313959+Tobkubos@users.noreply.github.com>
2024-04-03 17:29:58 +0200

    qwe

---
+++ WOT API/test.py
@@ -0,0 +1,48 @@
+import requests
+import json
+
+def get_clan_ratings():
+    url = "https://api.worldoftanks.eu/wot/clanratings/top/"
+    params = {
+        "application_id": "e5ebfe7a7df5498e28e61d230091cf09",
+        "rank_field": "fort_elo_rating_10",
+        "language": "pl",
+        "fields": "clan_id,clan_tag",
+        "limit": 999
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print("Błąd podczas pobierania danych:", response.status_code)
+        return None
+
+def extract_clan_ids(data):
+    if "data" in data:
+        clan_data = data["data"]
+        clan_ids = [clan["clan_id"] for clan in clan_data]
+        return clan_ids
+    else:
+        print("Brak danych o klanach w odpowiedzi.")
+        return None
+
+def save_to_json(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+# Wywołanie funkcji pobierającej dane
+clan_ratings_data = get_clan_ratings()
+
+if clan_ratings_data:
+    # Wyodrębnienie clan_id z danych
+    clan_ids = extract_clan_ids(clan_ratings_data)
+    
+    if clan_ids:
+        # Zapisanie clan_id do pliku JSON
+        save_to_json(clan_ids, "clan_ids.json")
+        print("clan_id zapisane do pliku 'clan_ids.json'.")
+else:
+    print("Brak danych do zapisania.")
---
+++ WOT API/test2.py
@@ -0,0 +1,41 @@
+import requests
+import json
+
+def get_clan_members(clan_id):
+    url = "https://api.worldoftanks.eu/wot/clans/info/"
+    params = {
+        "application_id": "e5ebfe7a7df5498e28e61d230091cf09",
+        "clan_id": clan_id,
+        "fields": "members",
+        "members_key": "id"
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print(f"Błąd podczas pobierania danych dla klanu o id {clan_id}: {response.status_code}")
+        return None
+
+def save_to_json(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+# Odczytanie clan ID z pliku JSON
+with open("clan_ids.json", "r") as f:
+    clan_id_data = json.load(f)
+    if isinstance(clan_id_data, list) and clan_id_data:
+        for clan_id in clan_id_data:
+            # Wywołanie funkcji pobierającej informacje o członkach klanu
+            clan_members_data = get_clan_members(clan_id)
+
+            if clan_members_data:
+                # Zapisanie danych o członkach do pliku JSON
+                save_to_json(clan_members_data, f"clan_{clan_id}_members.json")
+                print(f"Informacje o członkach klanu o id {clan_id} zapisane do pliku 'clan_{clan_id}_members.json'.")
+            else:
+                print(f"Brak danych o członkach dla klanu o id {clan_id}.")
+    else:
+        print("Nieprawidłowy format danych w pliku JSON lub brak danych.")
