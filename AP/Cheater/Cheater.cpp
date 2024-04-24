54b64576c4a753bcd7abb1ebc1b0ebc97a54a45f
Tobkubos <128313959+Tobkubos@users.noreply.github.com>
2024-04-25 00:13:42 +0200

    qweqeqwewqeqwe

--- AP/Cheater/Cheater.cpp
+++ AP/Cheater/Cheater.cpp
@@ -4,28 +4,49 @@
 
 using namespace std;
 
-// Function to convert card value to integer
-int convertValue(char value) {
-    if (value >= '2' && value <= '9') {
-        return value - '0'; // Convert digit char to int
-    } else if (value == 'T') {
-        return 10;
-    } else if (value == 'J') {
-        return 11;
-    } else if (value == 'Q') {
-        return 12;
-    } else if (value == 'K') {
-        return 13;
-    } else if (value == 'A') {
-        return 14;
+void Converter(vector<pair<int,int>> &cards, char a, char b){
+
+            pair<int,int> card;
+
+            if(a == '2') card.first = 2;
+            if(a == '3') card.first = 3;
+            if(a == '4') card.first = 4;
+            if(a == '5') card.first = 5;
+            if(a == '6') card.first = 6;
+            if(a == '7') card.first = 7;
+            if(a == '8') card.first = 8;
+            if(a == '9') card.first = 9;
+
+            if(a == 'T') card.first = 10;
+            if(a == 'J') card.first = 11;
+            if(a == 'Q') card.first = 12;
+            if(a == 'K') card.first = 13;
+            if(a == 'A') card.first = 14;
+
+            if(b == 'H') card.second = 3;
+            if(b == 'S') card.second = 2;
+            if(b == 'D') card.second = 1;
+            if(b == 'C') card.second = 0;
+            cards.push_back(card);
+}
+
+void SetCardsInProperOrder(vector<pair<int,int>> &adam_cards ,vector<pair<int,int>> &eve_cards ){
+    for(int i =0; i<adam_cards.size(); i++){
+        int iter = 0;
+        while(eve_cards[i].first < adam_cards[i].first && iter < adam_cards.size()-1){
+            pair<int,int> temp = eve_cards[i];
+            eve_cards.erase(eve_cards.begin()+i);
+            eve_cards.push_back(temp);
+            iter++;
+        }
+        //TODO: SPRAWDZ GEN KARTY, DAJ +1 JEZELI JEST, JEZELI NIE TO +2 JEZELI NIE TO +3
     }
-    return 0;
+
+    for(int i =0; i<adam_cards.size(); i++){
+        cout<<adam_cards[i].first<<" "<<adam_cards[i].second<<"  ///  "<<eve_cards[i].first<< " "<<eve_cards[i].second<<endl;
+    }
 }
 
-// Function to calculate points for Eve given Adam's cards and Eve's cards
-void calculatePoints(const vector<pair<char, char>>& adam_cards, const vector<pair<char, char>>& eve_cards) {
-
-}
 
 int main() {
     int cases;
@@ -36,18 +57,34 @@ int main() {
         cin >> cards;
 
         //Adam
-        vector<pair<char, char>> adam_cards(cards);
+        vector<pair<int, int>> adam_cards;
+        vector<pair<int,int>> eve_cards;
         for (int i = 0; i < cards; i++) {
-            cin >> adam_cards[i].first >> adam_cards[i].second;
+            char a,b;
+            cin>>a>>b;
+            Converter(adam_cards, a,b);
+
+        }
+        for (int i = 0; i < cards; i++) {
+            char c,d;
+            cin>>c>>d;
+            Converter(eve_cards, c,d);
+
+        }
+        sort(adam_cards.begin(), adam_cards.end());
+        sort(eve_cards.begin(), eve_cards.end());
+
+        //
+        for(int i = 0; i<adam_cards.size(); i++){
+            cout<<"KARTA ADAMA "<<adam_cards[i].first<<" : "<<adam_cards[i].second<<endl;
         }
 
-        //Eve
-        vector<pair<char, char>> eve_cards(cards);
-        for (int i = 0; i < cards; i++) {
-            cin >> eve_cards[i].first >> eve_cards[i].second;
+        for(int i = 0; i<eve_cards.size(); i++){
+            cout<<"KARTA EVY "<<eve_cards[i].first<<" : "<<eve_cards[i].second<<endl;
         }
+        //
 
-        calculatePoints(adam_cards, eve_cards);
+        SetCardsInProperOrder(adam_cards,eve_cards);
     }
 
     return 0;
---
+++ AP/Cheater/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+
+    char text1;
+    cin>>text1;
+    cout<<text1;
+    return 0;
+}
