cfa7de06c17e7ba4a6865daa96be7c31fb951e2f
Tobkubos <128313959+Tobkubos@users.noreply.github.com>
2024-05-08 20:25:19 +0200

    java combobox fun

--- JAVA/Zaj28_04/src/MyFrame.java
+++ JAVA/Zaj28_04/src/MyFrame.java
@@ -5,54 +5,107 @@ import java.awt.event.ActionListener;
 
 public class MyFrame extends JFrame {
     private JComboBox<String> cb;
+    public JPanel jp2;
+    public JPanel jp3;
+    public ButtonGroup bg1;
     private JLabel l;
+    int FontStyle = Font.PLAIN;
+    int FontSize = 30;
+    Font font;
+    String FontName = "Arial";
+
     MyFrame(){
         //frame
-        this.setVisible(true);
-        this.setSize(400,400);
+        this.setSize(400,600);
         this.setTitle("ComboBoxTest");
-        l = new JLabel("Testowy Tekst");
-        this.add(l, BorderLayout.CENTER);
+        l = new JLabel("PENIS");
+
+        font = new Font(FontName,FontStyle,FontSize);
+        l.setFont(font);
+        l.setHorizontalAlignment(SwingConstants.CENTER);
+        l.setVerticalAlignment(SwingConstants.CENTER);
 
         //Combo box
         cb = new JComboBox<>();
-        cb.addItem("Serif");
-        cb.addItem("SansSerif");
-        cb.addItem("Monospaced");
-
-        //listener
-        ActionListener sluchacz = event -> {
-            ChangeFont(cb.getItemAt(cb.getSelectedIndex()));
-        };
-        cb.addActionListener(sluchacz);
-        this.add(cb, BorderLayout.NORTH);
+        cb.setPreferredSize(new Dimension(200, 30));
+        ComboboxAdder("Monospaced");
+        ComboboxAdder("Serif");
+        ComboboxAdder("SansSerif");
 
         //radio buttons
-        ButtonGroup bg1 = new ButtonGroup();
-        JRadioButton jb1 = new JRadioButton("Small", null, false);
-        JRadioButton jb2 = new JRadioButton("Medium", null, false);
-        JRadioButton jb3 = new JRadioButton("Large", null, false);
-        JRadioButton jb4 = new JRadioButton("XLarge", null, false);
-        JRadioButton jb5 = new JRadioButton("XXLarge", null, false);
-
-        JPanel jp2 = new JPanel();
+        bg1 = new ButtonGroup();
+        jp2 = new JPanel();
         jp2.setLayout(new GridLayout(1,5));
-        this.add(jp2, BorderLayout.SOUTH);
 
-        jp2.add(jb1);
-        jp2.add(jb2);
-        jp2.add(jb3);
-        jp2.add(jb4);
-        jp2.add(jb5);
+        RadioButtonCreator("small dick", 10);
+        RadioButtonCreator("twojego starego", 25);
+        RadioButtonCreator("giga paruwa", 50);
 
-        bg1.add(jb1);
-        bg1.add(jb2);
-        bg1.add(jb3);
-        bg1.add(jb4);
-        bg1.add(jb5);
+
+        //checkboxes
+        jp3 = new JPanel();
+        CheckBoxCreator("Bold", Font.BOLD);
+        CheckBoxCreator("Plain", Font.PLAIN);
+        CheckBoxCreator("Italic", Font.ITALIC);
+
+        //ustawienie
+        JPanel jp1 = new JPanel();
+        jp1.setLayout(new GridLayout(2,1));
+
+        this.add(cb, BorderLayout.NORTH);
+        this.add(l, BorderLayout.CENTER);
+        jp1.add(jp2);
+        jp1.add(jp3);
+        this.add(jp1, BorderLayout.SOUTH);
+        this.setVisible(true);
     }
 
-    public void ChangeFont(String FontName){
-        l.setFont(new Font(FontName, Font.PLAIN, 40));
+    public void ComboboxAdder(String name){
+        cb.addItem(name);
+        //listener
+        ActionListener sluchacz = event -> {
+            FontName = cb.getItemAt(cb.getSelectedIndex());
+            updateFont();
+        };
+        cb.addActionListener(sluchacz);
+    }
+
+    public void RadioButtonCreator(String name, int size){
+            JRadioButton jrb = new JRadioButton(name,null,false);
+            ActionListener sluchacz = event -> {
+                ChangeSize(size);
+            };
+        jrb.addActionListener(sluchacz);
+        jp2.add(jrb);
+        bg1.add(jrb);
+    }
+    public void CheckBoxCreator(String name, int fs){
+        JCheckBox jcb = new JCheckBox(name, null, false);
+        ActionListener sluchacz = event -> {
+            JCheckBox clickedCheckbox = (JCheckBox) event.getSource();
+            if (clickedCheckbox.isSelected()) {
+                for (Component component : jp3.getComponents()) {
+                    if (component instanceof JCheckBox && component != clickedCheckbox) {
+                        ((JCheckBox) component).setSelected(false);
+                    }
+                }
+                ChangeStyle(fs);
+            }
+        };
+        jcb.addActionListener(sluchacz);
+        jp3.add(jcb);
+    }
+
+    public void ChangeStyle(int style){
+        FontStyle = style;
+        updateFont();
+    }
+    public void ChangeSize(int size){
+        FontSize = size;
+        updateFont();
+    }
+    public void updateFont() {
+        font = new Font(FontName, FontStyle, FontSize);
+        l.setFont(font);
     }
 }
