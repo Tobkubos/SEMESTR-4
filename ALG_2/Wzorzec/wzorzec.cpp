1f9272f6659017299d26bb54911c5ec6b81cdc54
Tobkubos <128313959+Tobkubos@users.noreply.github.com>
2024-05-15 23:06:29 +0200

    karp Rabin alg

--- ALG_2/Wzorzec/wzorzec.cpp
+++ ALG_2/Wzorzec/wzorzec.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -11,8 +12,8 @@ void ShowAll(vector<vector<string>> a, string code, string decoded){
         }
         cout<<endl;
     }
-    cout<<code<<endl;
-    cout<<decoded<<endl;
+    cout<<"code    "<<code<<endl;
+    cout<<"decoded "<<decoded<<endl;
 }
 
 vector<string> Convert(string a){
@@ -46,6 +47,43 @@ string Decode(vector<vector<string>> codes, string code){
     return Decoded;
 }
 
+//KARP RABIN
+string KarpRabin(string text, string pattern){
+    
+    int patternHash = 0;
+    for (char c : pattern) {
+        patternHash += static_cast<int>(c);
+    }
+
+    cout<<"Pattern Hash value "<<patternHash<<endl;
+    int count = 0;
+    
+    for(int i = 0; i<text.size() - pattern.size()+1; i++){
+        string check = text.substr(i,pattern.size());
+
+        int checkHash = 0;
+        for (char c : check) {
+            checkHash += static_cast<int>(c);
+        }
+
+        bool isGood = true;
+        if(patternHash == checkHash){
+            for(int j =0; j<pattern.size(); j++){
+                if(pattern[j] != check[j]){
+                    isGood = false;
+                }
+            }
+            if(isGood){
+                cout << "Znaleziono w " << i << endl;
+                count++;
+                if (count == 2)
+                    return pattern;
+            }
+        }
+    }
+    return "brak";
+}
+
 int main(){
 
     int cases = 0;
@@ -66,6 +104,18 @@ int main(){
         cin >> code;
         string decoded = Decode(AllCodes, code);
         ShowAll(AllCodes, code, decoded);
+
+
+        decoded = "CCCACCCACCACACACACAAAACAC";
+        int index = decoded.size()/2;
+
+        while(index>1){
+            for(int j = 0; j<decoded.size()-index+1; j++){
+                string pattern = decoded.substr(j, index);
+                cout<<KarpRabin(decoded,pattern)<<endl;
+            }
+            index--;
+        }
     }
     return 0;
 }
